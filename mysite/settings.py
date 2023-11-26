e366544308b933558241b9418cca29a09375c3ef
jcarrenoa <jcarrenoa@uninorte.edu.co>
2023-11-26 12:28:14 -0500

    ALLOWED_HOSTS Agregado a las configuraciones

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,6 +135,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = 'index'
 
+ALLOWED_HOSTS = ['proyectofinalbd.azurewebsites.net']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
