f1f979ab0b11a056b2c603a0bfc8e29cc5a39f72
jcarrenoa <jcarrenoa@uninorte.edu.co>
2023-11-26 13:46:50 -0500

    Lo mismo que los commits de abajo...

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'myapp/static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -136,3 +136,4 @@ ALLOWED_HOSTS = ['proyectofinalbd.azurewebsites.net', '127.0.0.1']
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://proyectofinalbd.azurewebsites.net/']
